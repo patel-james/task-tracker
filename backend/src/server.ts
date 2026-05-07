f573be0d03ed126b313f113a6e5b7ad1e4963619
patel-james <pateljames866@gmail.com>
2026-05-06 21:47:26 -0500

    First bakcned commit

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+import express, {Request, Response} from 'express'
+const app = express()
+const PORT: number = Number(process.env.PORT)
+
+app.use(express.json())
+
+app.get('/test', (req: Request, res: Response) => {
+    res.status(200).send("task tracker api is running ;)")
+})
+
+app.listen(PORT, () => {
+    console.log(`The app is running on http://localhost:${PORT}`)
+})
