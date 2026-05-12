15889d90ead9b9796cc651235b76debd58125b19
patel-james <pateljames866@gmail.com>
2026-05-11 19:28:40 -0500

    Add signup and login endpoints

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,28 @@
 import 'dotenv/config'
 import express, {Request, Response} from 'express'
+import pool from './db/connection.js'
+import authRoutes from './routes/authRoutes.js'
 const app = express()
 const PORT: number = Number(process.env.PORT) || 8000
 
 app.use(express.json())
 
 app.get('/test', (req: Request, res: Response) => {
-    res.status(200).send("task tracker api is running ;)")
+    res.status(200).json({message: "task tracker api is running ;)"})
 })
+
+app.get('/test-db', async (req: Request, res: Response) => {
+    try{
+        const [row] = await pool.query("SELECT 1 As Good")
+        return res.status(200).json({Message: row})
+    } catch (err){
+        res.status(500).json({Message: "Failed to connect to DB"})
+        console.log(err)
+    }
+})
+
+app.use('/api/auth', authRoutes)
+
 
 app.listen(PORT, () => {
     console.log(`The app is running on http://localhost:${PORT}`)
